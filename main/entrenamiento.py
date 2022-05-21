8238b3ee90be381f7608b783297e34c7a242a7a8
RodolfoFigueroa <4rodolfofigueroa2@gmail.com@>
2022-05-21 14:22:50 -0500

    Initial commit

---
+++ main/entrenamiento.py
@@ -0,0 +1,18 @@
+from modelo import RedNeuronal
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+from preprocesamiento import train_data_loader, validation_data_loader, \
+                             class_weights
+from constantes import checkpoints_dir
+
+tb_logger = pl_loggers.TensorBoardLogger(save_dir="../logs/")
+
+model = RedNeuronal(class_weights)
+trainer = pl.Trainer(accelerator="gpu", precision = 16, 
+                    default_root_dir = checkpoints_dir, 
+                    callbacks = [EarlyStopping(monitor = "val_loss", mode = "min")],
+                    logger = tb_logger)
+trainer.fit(model, train_data_loader, validation_data_loader)
+trainer.save_checkpoint(checkpoints_dir + "/final.ckpt")
---
+++ requirements_mano.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu114
+pip install pandas
+pip install pytorch-lightning
+pip install jupyterlab
+pip install sklearn
+pip install matplotlib
+pip install torchmetrics
