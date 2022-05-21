048c2491b27dde6dd4b1ab49de99e80aa6c65c7a
Rodolfo Figueroa Soriano <rodfiso@ciencias.unam.mx>
2022-05-21 15:22:26 -0500

    Update constantes.py

--- main/constantes.py
+++ main/constantes.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 main_dir = Path(__file__).parent / "..data/"
+main_dir = main_dir.resolve()
 print(main_dir)
 data_dir = main_dir / "/imagenes_train"
 dest_dir = main_dir / "/imagenes_train_mod"
