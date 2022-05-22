ac7aa4a10d385463327e5c50d0e55a2d1ced789c
Rodolfo Figueroa Soriano <rodfiso@ciencias.unam.mx>
2022-05-21 21:34:26 -0500

    Update acomodar_imagenes.py

--- main/acomodar_imagenes.py
+++ main/acomodar_imagenes.py
@@ -7,7 +7,7 @@ from lectura_data import metadata
 label = metadata["label"].unique()
 
 for i in label:
-    os.mkdir(dest_dir + str(i))
+    os.mkdir(dest_dir + "/" + str(i))
     imagenes = metadata.loc[metadata['label'] == i, 'image_id']
     for id in imagenes:
         shutil.copyfile((data_dir + "/" + id + ".jpg"), 
