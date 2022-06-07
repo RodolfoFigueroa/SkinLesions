189ca0a5f74c458d184461e4b3b619d177e5db26
Rodolfo Figueroa Soriano <rodfiso@ciencias.unam.mx>
2022-06-06 22:35:55 -0500

    Update lectura_data.py

--- main/lectura_data.py
+++ main/lectura_data.py
@@ -5,13 +5,13 @@ from constantes import main_dir
 metadata = pd.read_csv(main_dir + '/datos/HAM10000_metadata')
 
 label_map = {
-    akiec: 0,
-    bcc: 0, 
-    bkl: 0, 
-    df: 0, 
-    mel: 1, 
-    nv: 0, 
-    vasc: 0
+    "akiec": 0,
+    "bcc": 0, 
+    "bkl": 0, 
+    "df": 0, 
+    "mel": 1, 
+    "nv": 0, 
+    "vasc": 0
 }
  
 metadata["label"] = metadata["dx"].map(label_map)
