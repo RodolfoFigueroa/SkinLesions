70ef91e3f25dd21943857bd5ff80cba53d6cdc10
Rodolfo Figueroa Soriano <rodfiso@ciencias.unam.mx>
2022-06-06 22:22:32 -0500

    Update acomodar_imagenes.py

--- main/acomodar_imagenes.py
+++ main/acomodar_imagenes.py
@@ -1,46 +1,16 @@
-import argparse
 import os
 import shutil
 
 from constantes import data_dir, dest_dir
 from lectura_data import metadata
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--binary', 
-    dest='binary', 
-    action='store_const',
-    const=True, 
-    default=False,
-    help='Whether to sort images for binary classification.'
-)
+labels = metadata["label"].unique()
 
-label = metadata["label"].unique()
-
-if binary:
-    os.mkdir(dest_dir + "/0")
-    os.mkdir(dest_dir + "/1")
-
-    imagenes_true = metadata.loc[metadata['label'] == 4, 'image_id']
-    imagenes_false = metadata.loc[metadata['label'] != 4, 'image_id']
-    
-    for id in imagenes_true:
+for i in labels:
+    os.mkdir(dest_dir + "/" + str(i))
+    imagenes = metadata.loc[metadata['label'] == i, 'image_id']
+    for id in imagenes:
         shutil.copyfile(
             (data_dir + "/" + id + ".jpg"), 
-            (dest_dir + "/1/" + id + ".jpg")
+            (dest_dir + "/" + str(i) + "/" + id + ".jpg")
         )
-    for id in imagenes_false:
-        shutil.copyfile(
-            (data_dir + "/" + id + ".jpg"), 
-            (dest_dir + "/0/" + id + ".jpg")
-        )
-
-else:
-    for i in label:
-        os.mkdir(dest_dir + "/" + str(i))
-        imagenes = metadata.loc[metadata['label'] == i, 'image_id']
-        for id in imagenes:
-            shutil.copyfile(
-                (data_dir + "/" + id + ".jpg"), 
-                (dest_dir + "/" + str(i) + "/" + id + ".jpg")
-            )
