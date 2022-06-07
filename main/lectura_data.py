fa3aeeb4e992d212841213607d983285dfdd7604
Rodolfo Figueroa Soriano <rodfiso@ciencias.unam.mx>
2022-06-06 22:12:30 -0500

    Update lectura_data.py

--- main/lectura_data.py
+++ main/lectura_data.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
-from sklearn.preprocessing import LabelEncoder
-
 from constantes import main_dir
 
 metadata = pd.read_csv(main_dir + '/datos/HAM10000_metadata')
 
-le = LabelEncoder()
-le.fit(metadata['dx'])
-print("Classes:", list(le.classes_))
+label_map = {
+    akiec: 0,
+    bcc: 1, 
+    bkl: 2, 
+    df: 3, 
+    mel: 4, 
+    nv: 5, 
+    vasc: 6
+}
  
-metadata['label'] = le.transform(metadata["dx"]) 
+metadata["label"] = metadata["dx"].map(label_map)
